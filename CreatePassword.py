b1532792bcd756c6d199b08d7d0954001289ac4e
t inoue <tinoue.insta201806@gmail.com>
2018-08-28 18:22:23 +0900

    add CSV write Function

--- CreatePassword.py
+++ CreatePassword.py
@@ -1,16 +1,22 @@
+def write_csv(list_pass):
+    with open('password.csv','a',encoding="utf-8") as f:
+        f.write(list_pass + '\n')
+
 import string
 import random
 
 wCount = 8 #文字数
 wRows = 10 #行数
 
-words = string.ascii_letters
+words = string.ascii_letters #ascii_letters　→ a～z,A～Z
 
 #password生成
 passwd = [''.join(random.choice(words) for i in range(wCount)) for x in range(wRows)]
 
 #出力処理
-print('------{}文字のパスワードを{}行を出力します。--------------'.format(wCount,wRows))
+#print('------{}文字のパスワードを{}行を出力します。--------------'.format(wCount,wRows))
+print('------{}文字のパスワードを{}行をCSV出力します。--------------'.format(wCount,wRows))
 for password in passwd:
-    print(password)
-print('------------------------')
+    write_csv(password)
+#print('------------------------')
+print('CSV出力が完了しました。')
